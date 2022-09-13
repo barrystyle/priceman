fe4d98ba5bb7ef71f0a8067fa4abbfa71097c97c
barrystyle <barrystyle@westnet.com.au>
2022-09-13 16:23:34 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### priceman
+
+## to use:
+
+update api_key, db_user and db_pass to suit your local setup
+
+setup a local cronjob to call the script once every minute or 15
---
+++ priceman.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+
+# update coin price in yiimp using foreign exchanges
+# barrystyle 13092022
+
+import requests, pymysql, os, sys, time, json
+
+def fetch_vs_currency(ticker, base, apikey):
+    base_url = 'https://min-api.cryptocompare.com/data/price?fsym=XXXXX&tsyms=YYYYY&api_key=ZZZZZ'
+    data_url = base_url.replace('XXXXX', ticker).replace('YYYYY', base).replace('ZZZZZ', apikey)
+    try:
+        t = requests.get(data_url)
+        r = json.loads(t.text)
+        return r[base.upper()]
+    except:
+        return None
+
+def update_local_currencies(base, apikey, dbname, username, password):
+    try:
+        connection = pymysql.connect(host='localhost',
+                                 user=username,
+                                 password=password,
+                                 database=dbname,
+                                 cursorclass=pymysql.cursors.DictCursor,
+                                 autocommit=True)
+        cursor = connection.cursor()
+
+        # batch all currencies
+        sql = 'SELECT * FROM `coins`'
+        cursor.execute(sql)
+        result = cursor.fetchall()
+
+        # update all at once
+        for item in result:
+            cur_id = item['id']
+            cur_ticker = item['symbol']
+            cur_price = fetch_vs_currency(cur_ticker, base, apikey)
+            sql = 'UPDATE coins ' + \
+                  'SET price = ' + str(cur_price) + ' ' + \
+                  'WHERE id = ' + str(cur_id)
+            cursor.execute(sql)
+            print ('updated ' + str(cur_ticker) + ' to ' + str(cur_price) + ' ' + base.upper())
+    except:
+        print ('error')
+
+###################
+base_currency = 'btc'
+api_key = ''            # get one from https://min-api.cryptocompare.com/
+db_name = 'yiimp'       # or yiimpfrontend
+db_user = ''
+db_pass = ''
+
+update_local_currencies(base_currency, api_key, db_name, db_user, db_pass)
